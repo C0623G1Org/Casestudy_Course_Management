04ea6f8a3c13365c2d9955beab589dfd0263f5de
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2023-10-03 19:49:11 +0700

    thanh up

--- course-management/src/main/java/com/example/coursemanagement/service/ICourseDetailContentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursemanagement.service;
-
-import com.example.coursemanagement.model.CourseDetailedContent;
-
-import java.util.List;
-
-public interface ICourseDetailContentService extends IService<CourseDetailedContent>{
-    List<CourseDetailedContent> selectByCourseContentId(int id);
-}
--- course-management/src/main/java/com/example/coursemanagement/service/ICoursePurchaseService.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursemanagement.service;
-
-import com.example.coursemanagement.model.Course;
-
-public interface ICoursePurchaseService {
-    Course displayCourse (int id);
-}
